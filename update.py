a9da980d4236492c73d9dad6db354da9bd9f6cee
Damien Saucez <damien.saucez@gmail.com>
2023-07-10 15:43:08 +0200

    pre update nodes

--- update.py
+++ update.py
@@ -5,10 +5,16 @@ import functools
 sequences = [{10, 11}, {8, 9}]
 
 ansible_inventory = {}
-ansible_inventory['worker0'] = "192.168.64.5"
-ansible_inventory['worker1'] = "192.168.64.6"
-ansible_inventory['worker2'] = "192.168.64.7"
-ansible_inventory['worker3'] = "192.168.64.33"
+ansible_inventory['worker0']='10.205.80.192'
+ansible_inventory['worker9']='10.205.80.87'
+ansible_inventory['worker1']='10.205.80.223'
+ansible_inventory['worker2']='10.205.80.165'
+ansible_inventory['worker3']='10.205.80.163'
+ansible_inventory['worker4']='10.205.80.222'
+ansible_inventory['worker5']='10.205.80.124'
+ansible_inventory['worker6']='10.205.80.198'
+ansible_inventory['worker7']='10.205.80.122'
+ansible_inventory['worker8']='10.205.80.185'
 
 update_sequence = {
   '1.25.2': '1.26.2',
@@ -33,6 +39,8 @@ def _correct_version(node):
   version = node['kubernetes']
   if version is not None:
     node['kubernetes'] = correction_table[version]
+
+  return node['kubernetes']
 
 def _inventory(G, nodes):
   group = {}
@@ -59,13 +67,27 @@ def _run_playbook(playbook, inventories=[], extras=[]):
 
 G = ig.Graph.Read_GML('usecase.gml')
 
+
+pre_queue = []
 # Add inventory name
 for node in G.vs:
   name = node['name']
   if name in ansible_inventory:
-    _correct_version(node)
+    version = _correct_version(node)
     node['inventory_name'] = ansible_inventory[name]
 
+    # #######
+    if version == '1.26.2':
+      print ('should pre-update {}'.format(name))
+      pre_queue.append(int(node['id']))
+
+inventory = _inventory(G, pre_queue)
+hosts_file='hosts_pre'
+with open(hosts_file, 'w') as f:
+  yaml.dump(inventory, f)
+
+_run_playbook(playbook='k8s-update.yaml', inventories=['inventories/blueprint/core/', 'hosts_pre'], extras=['@params.blueprint.core.yaml', 'update_version={}'.format('1.26.2')])
+# #######
 # ###################################################################
 
 
